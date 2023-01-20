9ea6b974fc8ea35486356ad8b8616fd354cba35d
EvanilsonP <pevanilson99@gmail.com>
2023-01-20 14:50:22 -0300

    Handling POST requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+// It enables the visualiation of the content created on postman in the terminal, if using console.log(req.body) (POST)
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -13,6 +16,21 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours
         }
     })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1 ].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
