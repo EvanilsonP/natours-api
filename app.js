59751087085c029b342345d3121e3cd0b67cb622
EvanilsonP <pevanilson99@gmail.com>
2023-01-20 14:25:28 -0300

    Handling GET requets

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:'Hello from the server side!', App: 'Natours'});
-});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint.');
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
