c01eb806450f7d3e3678a114927fb20842c105f0
EvanilsonP <pevanilson99@gmail.com>
2023-01-20 14:00:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:'Hello from the server side!', App: 'Natours'});
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint.');
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
