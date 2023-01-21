e945079becd7c952bd1fa2270d0614cec5a7b1a2
EvanilsonP <pevanilson99@gmail.com>
2023-01-21 19:45:13 -0300

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -55,4 +55,20 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tour/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length) { // or if(!tour)
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID.'
+        })
+    };
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here.>'
+        }
+    });
+});
+
 app.listen(port, () => console.log(`App running on port ${port}`));
